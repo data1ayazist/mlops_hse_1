e1f942484455a0a3423e4b659a7e8cb11afe8da7
data1ayazist <114865945+data1ayazist@users.noreply.github.com>
2024-11-12 07:00:53 +0300

    +poetry+gRPC+readme

--- train_model.py
+++ train_model.py
@@ -4,6 +4,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def train_model(model_type, data: pd.DataFrame, params: dict = None):
+    """
+    Обучает модель на основе предоставленных данных и параметров.
+
+    Args:
+        model_type (str): Тип модели, которую нужно создать. Поддерживаемые типы: 'SVC', 'RandomForest'.
+        data (pd.DataFrame): Данные для обучения, где последний столбец является целевой переменной.
+        params (dict, optional): Гиперпараметры для настройки модели. По умолчанию None.
+
+    Raises:
+        ValueError: Если указанный тип модели не поддерживается.
+
+    Returns:
+        model: Обученная модель.
+    """
     # Предполагаем, что последний столбец - это целевая переменная
     X = data.iloc[:, :-1]  # Все столбцы, кроме последнего
     y = data.iloc[:, -1]   # Последний столбец
@@ -13,7 +27,7 @@ def train_model(model_type, data: pd.DataFrame, params: dict = None):
     elif model_type == 'RandomForest':
         model = RandomForestClassifier
     else:
-        raise ValueError("Неподдерживаемый тип модели. Используйте 'SVC' или 'RandomForest'.")
+        raise ValueError("Неподдерживаемый тип модели. ")
 
     # Настройка гиперпараметров с помощью GridSearchCV
     print(params)
@@ -23,6 +37,16 @@ def train_model(model_type, data: pd.DataFrame, params: dict = None):
     return trained_model
 
 def retrain_model(trained_model, new_data):
+    """
+    Переобучает существующую модель на новых данных.
+
+    Args:
+        trained_model: Обученная модель, которую нужно переобучить.
+        new_data (pd.DataFrame): Новые данные для переобучения, где последний столбец является целевой переменной.
+
+    Returns:
+        model: Переобученная модель.
+    """
     X = new_data.iloc[:, :-1]  # Все столбцы, кроме последнего
     y = new_data.iloc[:, -1]   # Последний столбец
     trained_model.fit(X, y)
